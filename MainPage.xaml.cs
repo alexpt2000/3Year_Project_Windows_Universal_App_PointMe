773895365eeb6cc722fc3f7613a9ea2ee7796706
alexpt2000gmit <g00317835@gmit.ie>
2017-02-27 19:25:41 +0000

    Add Flymenu and 3D maps

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -61,13 +61,6 @@ namespace PointMe
 
         }
 
-        private void HamburgerButton_Click(object sender,RoutedEventArgs args)
-        {
-            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
-        }
-
-
-
         private async void InitializeLocator()
         {
             var userPermission = await Geolocator.RequestAccessAsync();
@@ -136,6 +129,111 @@ namespace PointMe
 
             msgDialog.Content = status;
             msgDialog.ShowAsync();
+        }
+
+/*
+
+        private async void ShowStreetsideView(object sender, TappedRoutedEventArgs e)
+        {
+            // Check if Streetside is supported.
+            if (mapWithMyLocation.IsStreetsideSupported)
+            {
+                // Find a panorama near Avenue Gustave Eiffel.
+                BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 48.858, Longitude = 2.295 };
+                Geopoint cityCenter = new Geopoint(cityPosition);
+                StreetsidePanorama panoramaNearCity = await StreetsidePanorama.FindNearbyAsync(cityCenter);
+
+                // Set the Streetside view if a panorama exists.
+                if (panoramaNearCity != null)
+                {
+                    // Create the Streetside view.
+                    StreetsideExperience ssView = new StreetsideExperience(panoramaNearCity);
+                    ssView.OverviewMapVisible = true;
+                    mapWithMyLocation.CustomExperience = ssView;
+                }
+            }
+            else
+            {
+                // If Streetside is not supported
+                ContentDialog viewNotSupportedDialog = new ContentDialog()
+                {
+                    Title = "Streetside is not supported",
+                    Content = "\nStreetside views are not supported on this device.",
+                    PrimaryButtonText = "OK"
+                };
+                await viewNotSupportedDialog.ShowAsync();
+            }
+        }
+*/
+
+
+
+
+
+        private void MenuFlyoutItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (mapWithMyLocation.Is3DSupported)
+            {
+                // Set the aerial 3D view.
+                mapWithMyLocation.Style = MapStyle.Aerial3DWithRoads;
+
+                // Specify the location.
+                BasicGeoposition hwGeoposition = new BasicGeoposition() { Latitude = 34.134, Longitude = -118.3216 };
+                Geopoint hwPoint = new Geopoint(hwGeoposition);
+
+                // Create the map scene.
+                MapScene hwScene = MapScene.CreateFromLocationAndRadius(hwPoint,
+                                                                                     80, /* show this many meters around */
+                                                                                     0, /* looking at it to the North*/
+                                                                                     60 /* degrees pitch */);
+                // Set the 3D view with animation.
+                mapWithMyLocation.TrySetSceneAsync(hwScene, MapAnimationKind.Bow);
+            }
+            else
+            {
+                // If 3D views are not supported, display dialog.
+                ContentDialog viewNotSupportedDialog = new ContentDialog()
+                {
+                    Title = "3D is not supported",
+                    Content = "\n3D views are not supported on this device.",
+                    PrimaryButtonText = "OK"
+                };
+                viewNotSupportedDialog.ShowAsync();
+            }
+
+        }
+
+        private async void MenuFlyoutItem_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            // Check if Streetside is supported.
+            if (mapWithMyLocation.IsStreetsideSupported)
+            {
+                // Find a panorama near Avenue Gustave Eiffel.
+                BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 48.858, Longitude = 2.295 };
+                Geopoint cityCenter = new Geopoint(cityPosition);
+                StreetsidePanorama panoramaNearCity = await StreetsidePanorama.FindNearbyAsync(cityCenter);
+
+                // Set the Streetside view if a panorama exists.
+                if (panoramaNearCity != null)
+                {
+                    // Create the Streetside view.
+                    StreetsideExperience ssView = new StreetsideExperience(panoramaNearCity);
+                    ssView.OverviewMapVisible = true;
+                    mapWithMyLocation.CustomExperience = ssView;
+                }
+            }
+            else
+            {
+                // If Streetside is not supported
+                ContentDialog viewNotSupportedDialog = new ContentDialog()
+                {
+                    Title = "Streetside is not supported",
+                    Content = "\nStreetside views are not supported on this device.",
+                    PrimaryButtonText = "OK"
+                };
+                await viewNotSupportedDialog.ShowAsync();
+            }
+
         }
     }
 }
